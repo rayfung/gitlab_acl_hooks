4b2f614d8e03f1fab52263d7a4c7aa8e951370ea
Ray Fung <posixfung@163.com>
2015-12-13 18:30:22 +0800

    实现 deny pattern

--- custom_hooks/gitlab_acl.py
+++ custom_hooks/gitlab_acl.py
@@ -5,6 +5,6 @@ acl_dict = dict()
 
 # Example rules
 #
-# acl_dict['zhangsan'] = dict(default='pass')
+# acl_dict['zhangsan'] = dict(default='pass', deny_pattern=r'^LICENSE$')
 # acl_dict['lisi']     = dict(default='deny', pass_pattern=r'^doc/')
 # acl_dict['wangwu']   = dict(default='deny', pass_pattern=r'.+\.psd$|.+\.fbx$')
